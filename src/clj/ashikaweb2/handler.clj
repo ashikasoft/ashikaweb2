d08be528512950a476cf139ffe3d26eab2e58dd4
kbsant <keansant@gmail.com>
2021-01-28 18:56:32 +0900

    remove env handling for heroku compatibility

--- src/clj/ashikaweb2/handler.clj
+++ src/clj/ashikaweb2/handler.clj
@@ -1,14 +1,14 @@
 (ns ashikaweb2.handler
-  (:require [ashikaweb2.env :refer [defaults]]
-            [ashikaweb2.route.home :refer [home-routes]]
-            [ashikasoft.webstack.handler :as webstack.handler]
-            [ashikasoft.webstack.middleware :as webstack.middleware]
-            [reitit.ring :as ring]
-            [integrant.core :as ig]))
+  (:require
+   [ashikaweb2.route.home :refer [home-routes]]
+   [ashikasoft.webstack.handler :as webstack.handler]
+   [ashikasoft.webstack.middleware :as webstack.middleware]
+   [reitit.ring :as ring]
+   [integrant.core :as ig]))
 
 (defn app [dict]
   (webstack.middleware/wrap-base
-    ((:middleware defaults)
+    (identity
      (ring/ring-handler
       (ring/router
         [(home-routes dict)])
