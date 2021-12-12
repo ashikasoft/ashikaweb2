c809eb032e12e0b1117b44a9816474584c01c48f
kbsant <keansant@gmail.com>
2021-12-12 10:24:49 +0900

    bump deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :url "http://www.ashikasoft.com"
 
   :dependencies [[ashikasoft/jdict "1.0.2"]
-                 [ashikasoft/webstack "0.1.2"]
-                 [clojure.java-time "0.3.2"]
-                 [org.clojure/clojure "1.10.2"]]
+                 [ashikasoft/webstack "0.1.7"]
+                 [clojure.java-time "0.3.3"]
+                 [org.clojure/clojure "1.10.3"]]
 
   :min-lein-version "2.0.0"
   
@@ -32,12 +32,12 @@
    :test          [:project/dev :project/test :profiles/test]
 
    :project/dev  {:jvm-opts ["-Dconf=dev-config.edn"]
-                  :dependencies [[binaryage/devtools "1.0.2"]
+                  :dependencies [[binaryage/devtools "1.0.4"]
                                  [doo "0.1.11"]
-                                 [expound "0.8.7"]
-                                 [pjstadig/humane-test-output "0.10.0"]
-                                 [prone "2020-01-17"]
-                                 [ring/ring-devel "1.8.2"]
+                                 [expound "0.8.10"]
+                                 [pjstadig/humane-test-output "0.11.0"]
+                                 [prone "2021-04-23"]
+                                 [ring/ring-devel "1.9.4"]
                                  [ring/ring-mock "0.4.0"]]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.23.0"]
                                  [lein-doo "0.1.11"]]
